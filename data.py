f05d68671e36c3b662244d056c89631f2b60f3b8
will543399 <williamz111@nycstudents.net>
2026-09-22 09:23:11 -0400

    Commit 7th new unit 2

---
+++ data.py
@@ -0,0 +1,19 @@
+
+x=6
+y=float(3)
+print(x,y)
+
+
+""" bill= int(input("how much was the bill"))
+tip=float(input("How much do you want to tip for the service"))
+print(bill + (bill*tip))
+ """
+values=[1,2,23,5,7,2,30,15]
+""" print(values) """
+for i in values:
+    print(i)
+x="this is a thing"
+y=x.split( )
+z=y[0]
+print(y)
+print(z)
