8a7142140b71410845a111feeb2b2a5ccd2c8d01
will543399 <williamz111@nycstudents.net>
2026-09-23 09:25:58 -0400

    9/23/26

--- data.py
+++ data.py
@@ -1,4 +1,5 @@
 
+'''
 x=6
 y=float(3)
 print(x,y)
@@ -16,4 +17,31 @@ x="this is a thing"
 y=x.split( )
 z=y[0]
 print(y)
+print(z)'''
+
+answer=input("give me a random sentence")
+y=answer.split( )
+if y 
+z=y[0]
+print(y)
 print(z)
+
+dayoftheweek=input("what day of the week is it?")
+if dayoftheweek=="Friday":
+    print ("Correct")
+else:
+    print("Incorrect") 
+students= ["Ellie","Ben","Preston"]
+students.append("Sofia") 
+print(students[-1])
+#gets the last of the list
+
+for student in students:
+    if student=="Ben":
+        print(f'we found{student}')
+
+x="test"
+print(f"hello{x}")
+temp=75
+
+
